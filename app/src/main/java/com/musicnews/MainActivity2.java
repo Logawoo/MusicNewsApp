1d28e3ec180fb203a92efcaa302df2fe9cead214
WOOLDRIDGE <lw0938022@otc.edu>
2023-02-15 08:28:14 -0600

    Initial commit

---
+++ app/src/main/java/com/musicnews/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.musicnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ImageButton button3 = (ImageButton)findViewById(R.id.imgbtn1);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/musicnews/MainActivity3.java
@@ -0,0 +1,25 @@
+package com.musicnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        ImageButton button3 = (ImageButton)findViewById(R.id.imgbtn2);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity3.this, MainActivity.class));
+            }
+        });
+    }
+}
